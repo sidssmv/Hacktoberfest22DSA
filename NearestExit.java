537a7b018446fabf46541bb6e3adb20bbe662311
Paras <parassaini2991@gmail.com>
2022-10-07 01:29:25 -0700

    Create NearestExit.java

---
+++ NearestExit.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0) {
+			
+         int s=sc.nextInt();
+         
+         
+         if(s<=50)
+        	 System.out.println("LEFT");
+         else
+        	 System.out.println("RIGHT");
+      }
+	}
+}
